f210cb55475d23fa6f67943adbe0104782c1fc3c
Dominic Mendoza Enriquez <dome.enriquez.up@phinmaed.com>
2024-07-09 17:09:44 +0800

    Lil bit

--- RayCast3D.gd
+++ RayCast3D.gd
@@ -17,11 +17,11 @@ func _process(delta):
 		var collision_intensity = max_distance - collision_distance
 		
 		# Adjust camera height based on collision intensity
-		var target_height = original_height + collision_intensity * 7.0 # Adjust the multiplier as needed
+		var target_height = original_height + collision_intensity * 20.0 # Adjust the multiplier as needed
 		camera_controller.global_transform.origin.y = lerp(camera_controller.global_transform.origin.y, target_height, 0.1) # Ensure smooth adjustment
 
 		print("Collision intensity: ", collision_intensity)
 	else:
-		print("No collision")
+		print("No collisiond")
 		# Reset camera height smoothly
 		camera_controller.global_transform.origin.y = lerp(camera_controller.global_transform.origin.y, original_height, 5.0 * delta)
--- SpringArm3D.gd
+++ SpringArm3D.gd
@@ -10,6 +10,3 @@ func _process(delta):
 	# Check if the spring arm has collided
 	if spring_length < max_length:
 		var collision_intensity = max_length - spring_length
-		print("Collision intensity: ", collision_intensity)
-	else:
-		print("No collision")
